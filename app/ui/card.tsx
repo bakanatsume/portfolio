deae2f8967dd258e050f30aaf727e5788f62dc39
Bakanatsume <chhantyalbipash@gmail.com>
2024-04-15 18:28:35 +0100

    href card

--- app/ui/card.tsx
+++ app/ui/card.tsx
@@ -21,27 +21,25 @@ export default function Card({
   projectDetails: ProjectCardProps;
 }) {
   return (
-    <div
-      className={clsx(
-        `h-1/2 border border-2 rounded-2xl shadow-lg sticky top-0 `,
-        {
-          "bg-gradient-to-b from-white to-gray-500": topGap === 14,
-          "bg-gradient-to-b from-white to-gray-500 ":
-            topGap === 24,
-          " bg-gradient-to-b from-white to-gray-500 ":
-            topGap === 32,
-        }
-      )}
-      style={{ top: `${topGap}vh` }}
-    >
-      <div className="p-6">
-        <h1 className="text-3xl font-bold ">{projectDetails.title}</h1>
-        <h3>{projectDetails.summary}</h3>
-        <div className="md:grid md:grid-cols-2 grid-flow-col ">
-          <div>
-            <p className="mt-10 text-justify">{projectDetails.description}</p>
-          </div>
-          <Link href={projectDetails.url ?? "#"} passHref>
+    <Link href={projectDetails.url ?? "#"} passHref>
+      <div
+        className={clsx(
+          `h-1/2 border border-2 rounded-2xl shadow-lg sticky top-0 `,
+          {
+            "bg-gradient-to-b from-white to-gray-500": topGap === 14,
+            "bg-gradient-to-b from-white to-gray-500 ": topGap === 24,
+            " bg-gradient-to-b from-white to-gray-500 ": topGap === 32,
+          }
+        )}
+        style={{ top: `${topGap}vh` }}
+      >
+        <div className="p-6">
+          <h1 className="text-3xl font-bold ">{projectDetails.title}</h1>
+          <h3>{projectDetails.summary}</h3>
+          <div className="md:grid md:grid-cols-2 grid-flow-col ">
+            <div>
+              <p className="mt-10 text-justify">{projectDetails.description}</p>
+            </div>
             <Image
               src={
                 projectDetails.imageUrl
@@ -53,9 +51,9 @@ export default function Card({
               height={100}
               className="mx-auto"
             />
-          </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
