ee750c80080ddd480efb1d694f3ac4320020d8e0
Bakanatsume <chhantyalbipash@gmail.com>
2024-04-15 18:31:15 +0100

    url added

--- app/ui/card.tsx
+++ app/ui/card.tsx
@@ -2,7 +2,6 @@
 
 import clsx from "clsx";
 import Image from "next/image";
-import Link from "next/link";
 import React from "react";
 
 export interface ProjectCardProps {
@@ -21,18 +20,18 @@ export default function Card({
   projectDetails: ProjectCardProps;
 }) {
   return (
-    <Link href={projectDetails.url ?? "#"} passHref>
-      <div
-        className={clsx(
-          `h-1/2 border border-2 rounded-2xl shadow-lg sticky top-0 `,
-          {
-            "bg-gradient-to-b from-white to-gray-500": topGap === 14,
-            "bg-gradient-to-b from-white to-gray-500 ": topGap === 24,
-            " bg-gradient-to-b from-white to-gray-500 ": topGap === 32,
-          }
-        )}
-        style={{ top: `${topGap}vh` }}
-      >
+    <div
+      className={clsx(
+        `h-1/2 border border-2 rounded-2xl shadow-lg sticky top-0 `,
+        {
+          "bg-gradient-to-b from-white to-gray-500": topGap === 14,
+          "bg-gradient-to-b from-white to-gray-500 ": topGap === 24,
+          " bg-gradient-to-b from-white to-gray-500 ": topGap === 32,
+        }
+      )}
+      style={{ top: `${topGap}vh` }}
+    >
+      <a href={projectDetails.url}>
         <div className="p-6">
           <h1 className="text-3xl font-bold ">{projectDetails.title}</h1>
           <h3>{projectDetails.summary}</h3>
@@ -53,7 +52,7 @@ export default function Card({
             />
           </div>
         </div>
-      </div>
-    </Link>
+      </a>
+    </div>
   );
 }
